408ae9591970c2ed66066bf5138c9d1032458441
Nguyễn Nhất Nguyên <nhatnguyen741@gmail.com>
2025-01-10 01:35:43 +0700

    - Remove unused class.

--- CreationalPatterns/Builder/KeyboardParts/Color.cs
+++
@@ -1,6 +0,0 @@
-namespace Builder.KeyboardParts;
-
-/// <summary>
-/// Represents the color part of the keyboard.
-/// </summary>
-internal sealed class Color(string code, string name) : Base(code, name);
