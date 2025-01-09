a40a5112528c894818870f9e442286159c7619dd
Nguyễn Nhất Nguyên <nhatnguyen741@gmail.com>
2025-01-10 01:37:25 +0700

    - Remove unused xml comment.

--- CreationalPatterns/Builder/KeyboardParts/Switch.cs
+++ CreationalPatterns/Builder/KeyboardParts/Switch.cs
@@ -3,6 +3,4 @@ namespace Builder.KeyboardParts;
 /// <summary>
 /// Represents the switch part of the keyboard.
 /// </summary>
-/// <param name="code"></param>
-/// <param name="name"></param>
 internal sealed class Switch(string code, string name) : Base(code, name);
